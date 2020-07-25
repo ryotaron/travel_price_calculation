fb05ca7652e3d5fde2927d6309059d97f166782e
ryotaron <cxmrf372@yahoo.co.jp>
2020-07-25 14:38:30 +0900

    旅行プランをハッシュにまとめて再度作成

--- travel_plan.rb
+++ travel_plan.rb
@@ -1,53 +1,53 @@
-travel_plans = ["沖縄旅行（¥10,000）","北海道旅行（¥20,000）", "九州旅行（¥15,000）"]
-plan_prices = [10000,20000,15000]
-
-travel_plans.each.with_index(1) do |plan,i|
-  puts "#{i}:#{plan} "
-end
+#ハッシュごとにプランをまとめ、配列に格納
+travel_plans = [
+  { place: "沖縄", price: 10000 },
+  { place: "北海道", price: 20000 },
+  { place: "九州", price: 15000 }  
+]
 
 while true do
-  puts "番号入力して下さい！"
-  plan_number = gets.to_i
-  
+  # 配列からハッシュ（プラン）を取り出す
+  puts "旅行プランを選択してください"
+  travel_plans.each.with_index(1) do |travel_plan,number|
+    puts "#{number}:#{travel_plan[:place]}旅行 (¥#{travel_plan[:price]})"
+  end
+  #旅行プランの選択入力
+  plan_number = gets.to_i 
   case plan_number
-    when 1 then
-      puts "#{travel_plans[0]} を選択しました"
-      break
-    
-    when 2 then
-      puts "#{travel_plans[1]} を選択しました"
-      break
-    
-    when 3 then
-      puts "#{travel_plans[2]} を選択しました"
-      break
-      
+    when 1..3
+      selected_travel_plan = travel_plans[plan_number -1] 
+      puts "#{selected_travel_plan[:place]}旅行が選択されました"
+      break 
     else
-      puts "正しくない数字が入力されました、再度入力して下さい"
+      puts "不正な値です。再度入力してください。"
   end
 end
+ 
+ # 参加人数の入力
+while true do 
+  puts "参加人数を入力してください！"
+  person_number = gets.to_i
 
-while true do
-  puts "何人で行きますか？"
-  travel_person_number = gets.to_i
+  total_price = selected_travel_plan[:price] * person_number 
+  discout_total_price = total_price * 0.9
 
-  puts "人数は  #{travel_person_number}人ですね"
+  fixed_disp = "#{selected_travel_plan[:place]}旅行で人数は#{person_number}人ですね"
 
-  travel_price = plan_prices[plan_number -1]
+  if person_number <= 0
+    puts "不正な値です"
 
-  if travel_person_number <= 0
-    puts "不正な値です再度入力して下さい"
-
-  elsif travel_person_number < 5
-    total_travel_price = travel_price * travel_person_number
-    puts "トータル#{total_travel_price}円ですね"
-    break
-
-  else 
-    total_travel_price = travel_price * travel_person_number
-    discount_total_price = total_travel_price * 0.9
-    puts "５人以上なので、１０%引き　トータル#{discount_total_price}円です"
-    break
+  elsif person_number <= 5
     
+    puts fixed_disp
+    puts "トータル#{total_price}円になります"
+    break
+
+  else person_number > 5
+    
+    puts fixed_disp
+    puts "10%引き価格で #{discout_total_price}円になります"   
+    break
+
   end
 end
+
