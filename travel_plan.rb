b3ea3036deacf8f9cc67d4ad874e8241893fda5b
ryotaron <cxmrf372@yahoo.co.jp>
2020-07-26 20:21:11 +0900

    プラン番号の取得と代金表示を分割

--- travel_plan.rb
+++ travel_plan.rb
@@ -6,48 +6,37 @@ travel_plans = [
 ]
 
 while true do
-  # 配列からハッシュ（プラン）を取り出す
+  # 配列からハッシュ（プラン）を取り出し→表示
   puts "旅行プランを選択してください"
   travel_plans.each.with_index(1) do |travel_plan,number|
     puts "#{number}:#{travel_plan[:place]}旅行 (¥#{travel_plan[:price]})"
   end
-  #旅行プランの選択入力
+
+  #旅行プランの取得
   plan_number = gets.to_i 
-  case plan_number
-    when 1..3
-      selected_travel_plan = travel_plans[plan_number -1] 
-      puts "#{selected_travel_plan[:place]}旅行が選択されました"
-      break 
-    else
-      puts "不正な値です。再度入力してください。"
-  end
+  # break if 1 <= plan_number && 3 >= plan_number
+  # break if (1..3).cover?(plan_number) 
+    break if (1..3).include?(plan_number)
+
+  puts "不正な値です。再度入力してください。"
 end
+
+selected_travel_plan = travel_plans[plan_number -1] 
+puts "#{selected_travel_plan[:place]}旅行が選択されました"
  
- # 参加人数の入力
-while true do 
+while true do
   puts "参加人数を入力してください！"
   person_number = gets.to_i
-
-  total_price = selected_travel_plan[:price] * person_number 
-  discout_total_price = total_price * 0.9
-
-  fixed_disp = "#{selected_travel_plan[:place]}旅行で人数は#{person_number}人ですね"
-
-  if person_number <= 0
-    puts "不正な値です"
-
-  elsif person_number <= 5
-    
-    puts fixed_disp
-    puts "トータル#{total_price}円になります"
-    break
-
-  else person_number > 5
-    
-    puts fixed_disp
-    puts "10%引き価格で #{discout_total_price}円になります"   
-    break
-
-  end
+    break if 1<= person_number
+  puts "不正な値です"
 end
+#各料金の計算と表示
+total_price = selected_travel_plan[:price] * person_number 
+discout_total_price = total_price * 0.9
 
+puts "#{selected_travel_plan[:place]}旅行で人数は#{person_number}人ですね"
+if person_number < 5
+  puts "トータル#{total_price}円になります"
+else
+  puts "10%引き価格でトータル#{discout_total_price}円になります"   
+end
