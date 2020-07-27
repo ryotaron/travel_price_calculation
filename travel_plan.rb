f9c8070c9d74355b7e291fd8c936465c947ebc32
ryotaron <cxmrf372@yahoo.co.jp>
2020-07-27 14:00:48 +0900

    メソッド内の不用に見える変数を削除

--- travel_plan.rb
+++ travel_plan.rb
@@ -1,14 +1,13 @@
-# メソッド定義
 #ハッシュごとにプランをまとめ、配列に格納
-def summarize_plan
-travel_plans = [
-  { place: "沖縄", price: 10000 },
-  { place: "北海道", price: 20000 },
-  { place: "九州", price: 15000 }  
-]
+def summarize_place_price
+  [
+    { place: "沖縄", price: 10000 },
+    { place: "北海道", price: 20000 },
+    { place: "九州", price: 15000 }  
+  ]
 end
 
-def user_select_plan(travel_plans)
+def select_plan(travel_plans)
   while true do
     # 配列からハッシュ（プラン）を取り出し→表示
     puts "旅行プランを選択してください"
@@ -29,25 +28,23 @@ end
 def disp_selected_plan(travel_plans,plan_number)
   selected_travel_plan = travel_plans[plan_number -1] 
   puts "#{selected_travel_plan[:place]}旅行が選択されました"
-  selected_travel_plan
+  selected_travel_plan  #メソッドの最後の値
 end
 
-def user_select_person_num
+def select_person_num
   while true do
-  puts "参加人数を入力してください！"
-  person_number = gets.to_i
-  break if 1<= person_number
-  puts "不正な値です"
+    puts "参加人数を入力してください！"
+    person_number = gets.to_i
+    break if 1<= person_number
+    puts "不正な値です"
   end
   person_number #メソッドの最後の値
 end
-
+  #基本料金の計算
 def calculate_price(selected_travel_plan,person_number)
-  #各料金の計算と表示
-  total_price = selected_travel_plan[:price] * person_number 
-  
+  selected_travel_plan[:price] * person_number 
 end
-
+  #割引価格の計算
 def calcutlate_discount_price(total_price)
   discout_total_price = total_price * 0.9
 end
@@ -59,14 +56,15 @@ def disp_price(selected_travel_plan,person_number,total_price,discout_total_pric
   else
     puts "10%引き価格でトータル#{discout_total_price}円になります"   
   end
-
 end
 
-# メソッド呼び出し
-travel_plans = summarize_plan
-plan_number = user_select_plan(travel_plans)
+#メソッド呼び出し
+travel_plans = summarize_place_price
+plan_number = select_plan(travel_plans)
 selected_travel_plan = disp_selected_plan(travel_plans,plan_number)
-person_number = user_select_person_num
+
+person_number = select_person_num
 total_price = calculate_price(selected_travel_plan,person_number)
 discout_total_price = calcutlate_discount_price(total_price)
+
 disp_price(selected_travel_plan,person_number,total_price,discout_total_price)
